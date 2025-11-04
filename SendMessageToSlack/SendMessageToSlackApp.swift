dc9e5dd730d23ea336e86828faf765bf6c360bee
leadream <leadream4@gmail.com>
2025-11-04 10:10:05 +0800

    Initial Commit

---
+++ SendMessageToSlack/SendMessageToSlackApp.swift
@@ -0,0 +1,13 @@
+//
+//  Created by hallee on 2025/11/4.
+
+import SwiftUI
+
+@main
+struct SendMessageToSlackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SendMessageToSlackTests/SendMessageToSlackTests.swift
@@ -0,0 +1,13 @@
+//
+//  Created by hallee on 2025/11/4.
+
+import Testing
+@testable import SendMessageToSlack
+
+struct SendMessageToSlackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
